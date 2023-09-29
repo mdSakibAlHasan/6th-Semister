09fd44d569c9deef9444658767988c55c330c398
mdSakibAlHasan <bsse1209@iit.du.ac.bd>
2023-09-29 23:28:36 +0600

    docker complete without nginx

--- Distributed Systems/LinkedIn/forntent/src/notification/Notification.js
+++ Distributed Systems/LinkedIn/forntent/src/notification/Notification.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
 import axios from "axios";
 import Navbar from "../main/Navbar.js";
